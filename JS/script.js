286482daceaa8f06d4df29d0b1f37293f25d91b8
YASHWANTH A <yashwanth@YASHWANTHs-MacBook-Air.local>
2026-02-17 22:48:10 +0530

    Added formatting tags file

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+function message() {
+    alert("Submitted Successfully")
+}
+
+const pi = 3.14
+console.log("The value of pi is: " + pi);
+
+const stname = "Abhi"
+let grade = "8th"
+let cgpa = 8.7
+let sem = "1st"
+
+
+console.log("The student " + stname + " in " + grade + " grade have secured cgpa of " + cgpa + " in his " + sem + " semester")
+
+let age = 22
+console.log(age >= 18 ? "Eligible" : "Not Eligible")
